45a352a652393341661237298943121ab6c0daa7
Kacper Knuth <75022499+Ghaspar009@users.noreply.github.com>
2021-07-06 21:32:54 +0200

    Add files via upload

---
+++ DBConnectConsolVersion.java
@@ -0,0 +1,42 @@
+package DatabasesProjects.First;
+
+import java.io.Console;
+import java.sql.*;
+
+public class DBConnectConsolVersion {
+    public static void main(String[] args) {
+        try{
+
+            String username, password;
+            Console console = System.console();
+
+            username = console.readLine("Username: ");
+            char[] pass = console.readPassword("Password: ");
+            password = new String(pass);
+
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://150.254.78.34:3306/s452689_project", username, password);
+            Statement statement = connection.createStatement();
+
+            String order;
+            order = console.readLine();
+
+            ResultSet resultSet = statement.executeQuery(order);
+            ResultSetMetaData resultSetMetaData = resultSet.getMetaData();
+            int columnNumber =  resultSetMetaData.getColumnCount();
+            while(resultSet.next()) {
+                String line = "";
+                for (int i = 1; i <= columnNumber; i++) {
+                    line += resultSet.getString(i) + " ";
+                }
+                System.out.println(line);
+            }
+
+            connection.close();
+
+        }
+        catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
